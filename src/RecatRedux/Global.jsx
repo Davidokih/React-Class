eb8916778091a2c60d57431b58a3f44bbbf1ec9a
Davidokih <davidyole023@gmail.com>
2024-03-13 11:56:13 +0100

    update

---
+++ src/RecatRedux/Global.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const Global = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = Global.actions
+
+export default Global.reducer
